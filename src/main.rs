7d4cdfe6dee23cd90a7e25d739a18391956503a0
Adam Dawkins <adam@dragondrop.uk>
2026-06-25 23:45:03 +0100

    Handle quit action from Editor

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 use crossterm::event::{read, Event, KeyCode};
 
-use rim::{Buffer, Editor, Terminal};
+use rim::{editor::EditorAction, Buffer, Editor, Terminal};
 
 fn main() {
     let contents = fs::read_to_string("foo.txt").unwrap();
@@ -19,13 +19,14 @@ fn run(terminal: Terminal, mut editor: Editor) {
     loop {
         match read().unwrap() {
             Event::Key(key_event) => match key_event.code {
-                KeyCode::Char('q') => break,
                 KeyCode::Char('j') => editor.move_cursor_down(),
                 KeyCode::Char('k') => editor.move_cursor_up(),
                 KeyCode::Char('l') => editor.move_cursor_right(),
                 KeyCode::Char('h') => editor.move_cursor_left(),
                 key => {
-                    editor.handle_keypress(key);
+                    if let Some(EditorAction::Quit) = editor.handle_keypress(key) {
+                        break;
+                    }
                 }
             },
             _ => {}
