4ad4485a222d1f5c875d57903129a520a2fa5c71
Adam Dawkins <adam@dragondrop.uk>
2026-06-25 22:35:13 +0100

    Add Editor

---
+++ src/editor.rs
@@ -0,0 +1,30 @@
+use crate::{Buffer, Cursor};
+
+pub struct Editor {
+    buffer: Buffer,
+    cursor: Cursor,
+    mode: EditorMode,
+}
+
+impl Editor {
+    pub fn new(buffer: Buffer) -> Self {
+        Editor {
+            buffer,
+            cursor: Cursor::default(),
+            mode: EditorMode::Normal,
+        }
+    }
+
+    pub fn buffer(&self) -> &Buffer {
+        &self.buffer
+    }
+
+    pub fn cursor(&self) -> &Cursor {
+        &self.cursor
+    }
+}
+
+enum EditorMode {
+    Normal,
+    Insert,
+}
