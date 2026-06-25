f02b986d181fad97a451bde851a8dd7a47b09df4
Adam Dawkins <adam@dragondrop.uk>
2026-06-25 23:41:30 +0100

    Use match on KeyCode in run instead of if statements

--- src/main.rs
+++ src/main.rs
@@ -18,23 +18,14 @@ fn run(terminal: Terminal, mut editor: Editor) {
 
     loop {
         match read().unwrap() {
-            Event::Key(key_event) => {
-                if key_event.code == KeyCode::Char('q') {
-                    break;
-                }
-                if key_event.code == KeyCode::Char('j') {
-                    editor.move_cursor_down();
-                }
-                if key_event.code == KeyCode::Char('k') {
-                    editor.move_cursor_up();
-                }
-                if key_event.code == KeyCode::Char('l') {
-                    editor.move_cursor_right();
-                }
-                if key_event.code == KeyCode::Char('h') {
-                    editor.move_cursor_left();
-                }
-            }
+            Event::Key(key_event) => match key_event.code {
+                KeyCode::Char('q') => break,
+                KeyCode::Char('j') => editor.move_cursor_down(),
+                KeyCode::Char('k') => editor.move_cursor_up(),
+                KeyCode::Char('l') => editor.move_cursor_right(),
+                KeyCode::Char('h') => editor.move_cursor_left(),
+                _ => {}
+            },
             _ => {}
         }
 
