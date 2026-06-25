d5269ed1251a987e77bdf9f86d1633cd97c96c23
Adam Dawkins <adam@dragondrop.uk>
2026-06-25 17:16:34 +0100

    f enable and disable raw mode around the output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use std::fs;
 
+use crossterm::terminal;
+
 fn main() {
     let buffer = fs::read_to_string("foo.txt").unwrap();
+    let _terminal = Terminal::new();
     println!("{}", buffer);
+}
+
+pub struct Terminal;
+
+impl Terminal {
+    pub fn new() -> Self {
+        terminal::enable_raw_mode().unwrap();
+        Terminal
+    }
+}
+
+impl Drop for Terminal {
+    fn drop(&mut self) {
+        terminal::disable_raw_mode().unwrap();
+    }
 }
