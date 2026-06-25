2b5a21dbe68782a598e30af505bd15bfd1fe6257
Adam Dawkins <adam@dragondrop.uk>
2026-06-25 20:35:43 +0100

    Stop left movement going beyond first column

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,10 @@ impl Cursor {
     }
 
     pub fn left(&mut self) {
+        if self.col == 0 {
+            return;
+        }
+
         self.col -= 1;
     }
 
@@ -31,5 +35,28 @@ impl Cursor {
 
     pub fn col(&self) -> u16 {
         self.col
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cursor_left_goes_left() {
+        let mut cursor = Cursor::new(1, 10);
+
+        cursor.left();
+
+        assert_eq!([cursor.row(), cursor.col()], [1, 9]);
+    }
+
+    #[test]
+    fn test_cursor_cannot_go_left_from_first_column() {
+        let mut cursor = Cursor::new(1, 0);
+
+        cursor.left();
+
+        assert_eq!([cursor.row(), cursor.col()], [1, 0]);
     }
 }
